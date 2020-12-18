8c087c4094799e79bd38d63760111a89adde5e29
Taha Azam <muhammadtahaazam@gmail.com>
2020-12-18 12:50:56 +0500

    Python All in one

---
+++ basic python/modifyString.py
@@ -0,0 +1,19 @@
+#Modify String
+
+S= "Hello, WoRLD"
+print(S.strip(" "))
+print(S.replace("H", "J"))
+print(S.split(","))
+
+a=36
+
+txt = "my age is {}"
+print(txt.format(a))
+
+w="wording"
+print(w.capitalize())
+print(w.casefold()) #String to lower
+print(w.center(5))
+
+print(w.translate('o'))
+
